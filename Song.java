162d90a1c1e1fc47f98ca2bf92bd6fa8e400d804
Greg Panossian <gregpanossian@MacBook-Pro-5.local>
2025-01-27 12:04:01 -0800

    Fininshed Song class

--- Song.java
+++ Song.java
@@ -1,30 +1,70 @@
 /**
- * File for a Song class to be used in the Playlist Project
- * @author
- * @version
+ * Song class to be used in Playlist Project
+ * @Greg Panossian
+ * @1-24-25
  */
-public class Song {
+public class Song 
+{
     //Fields-- what information do we want each Song to store?
-
-
-
+    private String title;
+    private int time; // in seconds
+    private String artist;
+    private boolean liked; 
 
     /**
-     * Constructor-- what information needs to be given to make a Song?
-     * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
+     * By default, the song is not liked (status = false)
      */
-
-
-
-
-
-     /**
+    public Song(String title, int time, String artist)
+    {
+        this.title = title;
+        this.time = time;
+        this.artist = artist;
+        this.liked = false;
+    }
+    
+    /**
       * Methods-- what will you want each Song to do?
       * Consider all the getter methods (getName, getArtist, etc.)
       * You should probably have a toString method to be able to get the information for the full
       * song easily as well!
       * What kind of mutator (setter) methods will you need?
       */
-
-
+    public String getTitle()
+    {
+         return title;
+    }
+    
+    public int getTime()
+    {
+        return time;
+    }
+    
+    public String getArtist()
+    {
+        return artist;
+    }
+    
+    public boolean getStatus()
+    {
+        return liked;
+    }
+    
+    //Setters are below
+    public void setLiked(boolean status)
+    {
+        liked = status;
+    }
+    
+    public String toMinutes()
+    {
+        int minutes = time / 60;
+        int seconds = time % 60;
+        return String.format("%d:%02d", minutes, seconds);
+    }
+    
+    public String toString()
+    {
+        return String.format("Title: %s | Artist: %s | Time: %s | Liked: %b", 
+                             title, artist, toMinutes(), liked);
+    }
 }
