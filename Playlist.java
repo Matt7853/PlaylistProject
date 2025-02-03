7e1a9ec70430146fd0dbb47bda5a7f9f60a1fa38
Matthew Bristing <matthew@bristing.com>
2025-02-03 12:30:44 -0800

    Completed Playlist and filled out PlaylistTester with tests

--- Playlist.java
+++ Playlist.java
@@ -1,23 +1,28 @@
-import java.util.ArrayList;
-
 /**
  * The Playlist class, which will keep track of a playlist of Song objects
  * Refer to the project description to make sure you have access to all available methods
+ * 
+ * @author Matthew and Greg
+ * @version 2025-01-27
  */
+
+import java.util.ArrayList;
+
 public class Playlist {
     /**
      * Fields-- This will likely just need to be the ArrayList of Songs. Reference our previous problems
      * (CarDealership, Zoo) for structure on how this will look
      */
-
-
+    private ArrayList<Song> playlist;
 
 
      /**
       * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
       * then use additional methods to add Songs in one-by-one
       */
-
+    public Playlist() {
+        playlist = new ArrayList<Song>();
+    }
 
       
 
@@ -31,4 +36,63 @@ public class Playlist {
        * Determining the total duration of all songs
        * Removing all unliked songs from the playlist (careful with this one!)
        */
+    public int find(String title) {
+        int index = 0;
+        for (Song i : playlist) {
+            if (i.getTitle().equals(title)) {
+                return index;
+            }
+            index++;
+        }
+        return 0;
+    }
+    public void addSong(Song newSong) {
+        playlist.add(newSong);
+    }
+    
+    public void addSong(String title, int length, String artist) {
+        playlist.add(new Song(title, length, artist));
+    }
+
+    public void removeSong(int index) {
+        playlist.remove(index);
+    }
+
+    public void changeStatus(String title) {
+        playlist.get(find(title)).setLiked(true);
+    }
+
+    public void printPlaylist() {
+        int count = 1;
+        for (Song i : playlist) {
+            System.out.println(count + ") " + i.toString());
+            count++;
+        }
+    }
+
+    public int getTotalTime() {
+        int total = 0;
+        for (Song i : playlist) {
+            total += i.getTime();
+        }
+        return total;
+    }
+
+    public void removeUnliked() {
+        for (int i = 0; i < playlist.size(); i++) {
+            if (!playlist.get(i).getStatus()) {
+                playlist.remove(i);
+            }
+        }
+    }
+
+    public void printLiked() {
+        int count = 1;
+        for (Song i : playlist) {
+            if (i.getStatus()) {
+            System.out.println(count + ") " + i.toString());
+            count++;
+            }
+        }
+    }
 }
--- PlaylistTester.java
+++ PlaylistTester.java
@@ -1,15 +1,17 @@
+import java.util.Arrays;
+
 /**
  * Sample of a tester file for the Playlist class. This file should demonstrate all the capability of your
  * playlist in the main method. You don't need to follow the testing specifications of this exactly
  * if you want to write your own separate tester instead.
  * Note that there's no need for a Scanner in this project-- all of the playlist can be 'hardcoded' into main.
- * @author
- * @version
+ * @author Matthew & Greg
+ * @version 2025-01-27
  */
 public class PlaylistTester {
     public static void main(String[] args) {
         System.out.println("Initializing a Playlist...\n");
-        //Make your playlist here
+        Playlist p1 = new Playlist();
 
         System.out.println("Adding songs to the Playlist...\n");
         /**
@@ -17,39 +19,56 @@ public class PlaylistTester {
          * is something like...
          * p.addSong(new Song(..., ..., ...))
          */
-
-
-
+        Song s1 = new Song("Twist", 59, "Korn");
+        Song s2 = new Song("Paranoid", 168, "Black Sabbath");
+        p1.addSong(s1);
+        p1.addSong(s2);
+        p1.addSong("Mr. Brightside", 223, "The Killers");
+        p1.addSong("Start a Fire", 192, "John Legend");
+        p1.addSong("As the World Caves In", 219, "Matt Maltese");
+        
+        
         System.out.println("Printing the songs...\n");
         //Print out all the songs in the playlist to verify it's working correctly
-
-
+        p1.printPlaylist();
+        
+        
         System.out.println("\nLiking the songs in position X, Y, Z, etc....\n");
         //Once your songs are 'liked', this should be reflected in the next printout
-
+        p1.changeStatus("Mr. Brightside");
+        p1.changeStatus("Twist");
+        p1.changeStatus("Paranoid");
+        
 
         System.out.println("Printing the songs...\n");
+        p1.printPlaylist();
 
 
         System.out.println("\nRemoving the song in position A, B, C, etc...\n");
-
+        p1.removeSong(3);
 
         System.out.println("Printing the songs...\n");
+        p1.printPlaylist();
 
 
         System.out.println("\nPrinting only the liked songs...\n");
         //Your Playlist should be able to do this without looping while in main!
-
+        p1.printLiked();
 
         System.out.println("\nPrinting the total duration of all songs...\n");
         //Note that the format should look something like minutes:seconds
-
+        int time = p1.getTotalTime();
+        int minutes = time / 60;
+        int seconds = time % 60;
+        System.out.println(String.format("%d:%02d", minutes, seconds));
 
         System.out.println("\nRemoving all unliked songs from the playlist...\n");
         //This should be doable with a single method call
-
+        p1.removeUnliked();
 
         System.out.println("Printing all songs...\n");
         //This should now look like only the liked songs list from before
+        p1.printPlaylist();
+
     }
 }
